f914e4fece8c349eb7f1d185b8cdffcc0cc81658
Lexx Utopia Olivia Damm <lexx.damm@gmail.com>
2021-06-08 12:49:02 +0200

    Delete dist directory

--- dist/routes/account.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-router.get('/:id', (req, res) => {
-    console.log("Get user");
-    res.send(`ID: ${req.params.id}`);
-});
-export default router;
-//# sourceMappingURL=account.js.map
