b5dfa662435f482ce89b7ea76b13a3bd0400b4ff
Lexx Damm <lexx.damm@gmail.com>
2021-06-09 16:32:07 +0200

    retrive user works

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -5,13 +5,11 @@ import { User } from '../types/user';
 const db = admin.firestore();
 
 export const getUser = async (request: express.Request, response: express.Response) => {
-    const id = request.params.id;
+    const id = request.params.id.trim();
     const doc = await db.collection('users').doc(id).get();
     if (!doc.exists) {
-        console.log('No such document!');
         response.send('No such document!');
     } else {
-        console.log('Document data:', doc.data());
-        response.send('Document data: ' + doc.data());
+        response.json(doc.data());
     }
 }
