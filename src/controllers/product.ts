c696fd8998128c37d0cfe10143cbd879a2582052
Lexx Damm <lexx.damm@gmail.com>
2021-06-12 14:35:19 +0200

    api works, next auth

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -10,7 +10,9 @@ export const getAllProducts = async (req: Request, res: Response) => {
     } else {
         let products: Array<Product> = [];
         docs.forEach((doc) => {
-            products.push(doc.data() as Product);
+            let product = doc.data() as Product;
+            product.id = doc.id;
+            products.push(product);
         });
         res.send(products);
     }
@@ -20,9 +22,31 @@ export const getProduct = async (req: Request, res: Response) => {
     const productId = req.params.p_id.trim();
     const doc = await db.collection('products').doc(productId).get();
     if (!doc.exists) {
-        res.send('No such document!');
+        res.send('No such product');
+    } else {
+        let product: Product = doc.data() as unknown as Product;
+        res.json(product);
+    }
+};
+
+export const updateProductQuantityInStock = async (req: Request, res: Response) => {
+    const productId = req.params.p_id.trim();
+    const operation = req.params.operation.trim();
+    const docRef = db.collection('products').doc(productId);
+    const doc = await docRef.get();
+    if (!doc.exists) {
+        res.send('No such product');
     } else {
         const product: Product = doc.data() as unknown as Product;
-        res.json(product);
+        if (operation === 'increment') {
+            product.stock++;
+        } else if (operation === 'deincrement') {
+            if (product.stock > 0) product.stock--;
+        } else {
+            res.send('Malformed operation');
+            return;
+        }
+        const result = await docRef.update(product);
+        res.send(result);
     }
 };
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAllProducts, getProduct } from '../controllers/product';
+import { getAllProducts, getProduct, updateProductQuantityInStock } from '../controllers/product';
 
 const router = Router();
 
 router.get('/', getAllProducts);
 router.get('/:p_id', getProduct);
+router.patch('/:p_id/stock/:operation', updateProductQuantityInStock);
 
 export default router;
